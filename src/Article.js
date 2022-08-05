a8d0d76fd5acd309878149797bae024b8f4421a7
Basia Stolarz <basiastolarz@Basias-MacBook-Pro.local>
2022-08-05 19:37:07 +0200

    Changed the UI library and configured ESlint.

--- src/Article.js
+++ src/Article.js
@@ -1,38 +1,49 @@
 import './app.scss'
 
+import * as React from 'react';
+import Card from '@mui/material/Card';
+import CardActions from '@mui/material/CardActions';
+import CardContent from '@mui/material/CardContent';
+import CardMedia from '@mui/material/CardMedia';
+import Button from '@mui/material/Button';
+import Typography from '@mui/material/Typography';
+import { Grid } from '@mui/material';
+
+
 const Article = ( articleData, index ) => {
   articleData= articleData.articleData;
     const title = articleData.title
     const author = articleData.author
-    const source = articleData.source.title
     const content = articleData.content
     const link = articleData.url
     const imageLink = articleData.urlToImage
 
   return (
-    <div
-      id={`recipe${index}`}
-      key={index}
-      className="mdc-card mdc-card--outlined my-card-content">
-        <div className="my-card__media mdc-card__media mdc-card__media--16-9">
-        <div className="mdc-card__media-content">
-          <img
-            src={imageLink}
-            alt={title}
-            className="mdc-card--image"
-          />
-        </div>
+    <Grid item key={index} xs={12} sm={6} md={4}>
 
-          <h4 >{title}</h4>
-          <p>{author} {source}</p>
-          <p>{content}</p>
-          <a href={link} target="_blank" rel="noreferrer">
-            <button className='mdc-button mdc-card__action mdc-card__action--button'>
-            <span className="mdc-button__label">Read more</span>
-            </button>
-          </a>
-      </div>
-    </div>
+    <Card  sx={{ maxWidth: 345 }}>
+    <CardMedia
+      component="img"
+      alt={title}
+      height="140"
+      image={imageLink}
+    />
+    <CardContent>
+      <Typography gutterBottom variant="h5" component="div">
+        {title}
+      </Typography>
+      <Typography variant="body1" color="text.secondary">
+        {author} 
+      </Typography>
+      <Typography variant="body2" color="text.primary">
+        {content}
+      </Typography>
+    </CardContent>
+    <CardActions>
+      <Button size="small" href={link} target="_blank" rel="noreferrer">Read More</Button>
+    </CardActions>
+  </Card>
+  </Grid>
   );
 };
 
---
+++ src/TopArticles.js
@@ -0,0 +1,39 @@
+import React, { useState, useEffect } from 'react';
+import { Grid, Container } from '@mui/material';
+import Article from './Article';
+
+function TopArticles() {
+  const [articles, setArticles] = useState([]);
+
+  let url = 'https://newsapi.org/v2/top-headlines?' +
+    'country=us&' +
+    'apiKey=644c0248558246f5929da6bafb4ba056';
+
+  useEffect(() => {
+    fetch(url)
+      .then((response) => {
+        return response.json()
+      })
+      .then((result) => {
+        setArticles(result.articles)
+      })
+
+  }, []);
+
+
+  if (articles.length > 0) {
+    return (
+      <Container sx={{ py: 8 }} maxWidth="lg">
+        <Grid container spacing={4}>
+          {articles.map((article, index) => {
+            return <Article articleData={article} key={index} />;
+          })}
+        </Grid>
+      </Container>
+    )
+  } else {
+    return <div className="App">No news for you</div>
+  }
+}
+
+export default TopArticles;
