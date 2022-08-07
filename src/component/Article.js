5882e12b5f79e62e2511b0b65518a481264d473d
JiaanZZZ <jiaanzhu0618@gmail.com>
2022-08-07 17:30:54 +0100

    add like to read more

--- src/component/Article.js
+++ src/component/Article.js
@@ -8,29 +8,27 @@ import CardMedia from "@mui/material/CardMedia";
 import Button from "@mui/material/Button";
 import Typography from "@mui/material/Typography";
 import { Grid } from "@mui/material";
+import FavoriteIcon from "@mui/icons-material/Favorite";
+import IconButton from "@mui/material/IconButton";
 
 const Article = ({ articleData, index }) => {
-  const title = articleData.title
+  const title = articleData.title;
   const author = articleData.author;
   const content = articleData.content;
   const link = articleData.url;
   let imageLink = articleData.urlToImage;
-  const defaultImageLink = 'https://images.unsplash.com/photo-1478940020726-e9e191651f1a?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=2940&q=80'
+  const defaultImageLink =
+    "https://images.unsplash.com/photo-1478940020726-e9e191651f1a?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=2940&q=80";
 
   if (imageLink === null) {
-    imageLink = defaultImageLink
+    imageLink = defaultImageLink;
   }
 
   if (author) {
     return (
       <Grid item key={index} xs={12} sm={6} md={4}>
         <Card sx={{ maxWidth: 345 }}>
-          <CardMedia
-            component="img"
-            alt={title}
-            height="200"
-            src={imageLink}
-          />
+          <CardMedia component="img" alt={title} height="200" src={imageLink} />
           <CardContent>
             <Typography gutterBottom variant="h6" component="div">
               {title}
@@ -46,6 +44,9 @@ const Article = ({ articleData, index }) => {
             <Button size="small" href={link} target="_blank" rel="noreferrer">
               Read More
             </Button>
+            <IconButton aria-label="add to favorites">
+              <FavoriteIcon />
+            </IconButton>
           </CardActions>
         </Card>
       </Grid>
@@ -67,6 +68,10 @@ const Article = ({ articleData, index }) => {
           <Button size="small" href={link} target="_blank" rel="noreferrer">
             Read More
           </Button>
+
+          <IconButton aria-label="add to favorites">
+            <FavoriteIcon />
+          </IconButton>
         </CardActions>
       </Card>
     </Grid>;
